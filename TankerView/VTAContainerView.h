b19e82a4f30f08403735fe483c1de2af4614822f
Simon Fairbairn <simon@line-in.co.uk>
2012-12-20 17:29:34 -0600

    Restoring from snapshot

---
+++ TankerView/VTAContainerView.h
@@ -0,0 +1,17 @@
+//
+//  VTAViewController.h
+//  TankerView
+//
+//  Created by Simon Fairbairn on 19/12/2012.
+//  Copyright (c) 2012 Simon Fairbairn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VTAViewController : UIViewController
+
+- (IBAction)button:(id)sender;
+- (IBAction)darkenSwitch:(id)sender;
+
+
+@end
